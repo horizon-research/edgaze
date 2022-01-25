5dc64c8a184324a6636c7faa8a66210d2b55fa2b
Yu Feng <yfeng28@ur.rochester.edu>
2022-01-24 21:01:11 -0800

    add new files event emulator and pipeline framework code

---
+++ edgaze/emulator.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python
+"""
+This is simplified event generator to generate events between two 
+temporally-adjacent images.
+
+@author: Yu Feng
+"""
+
+import cv2
+import numpy as np
+import os
+
+COLOR_MAX = 255
+
+
+class SimpleEmulator(object):
+    """This is a simplified event emulator
+
+    """
+    def __init__(self,
+                 blur_size=3,
+                 clip_val=10,
+                 threshold_ratio=0.3):
+        """ Init.
+
+        Args:
+            blur_size: the gaussian blur size apply to input images
+            clip_val: clip all low pixel value to this number
+            threshold ratio: the threshold ratio to activate an event
+        """
+        self.blur_size = blur_size
+        self.clip_val = clip_val
+        self.ratio = threshold_ratio
+
+        self.clipped_prev_frame = None
+
+    def generate_events(self, curr_frame):
+        """
+            generate events as an list, and diff map as a numpy array
+        """
+        # blur the image first
+        curr_frame = cv2.blur(curr_frame, (self.blur_size, self.blur_size))
+        clipped_curr_frame = np.clip(curr_frame, self.clip_val, COLOR_MAX)
+
+        events = []
+        diff = None
+
+        if self.clipped_prev_frame is not None:
+            diff = clipped_curr_frame - self.clipped_prev_frame
+            ratio_frame = np.abs(diff)/self.clipped_prev_frame
+            diff = np.clip(ratio_frame, self.ratio, 1) - self.ratio
+            
+            for row in range(diff.shape[0]):
+                for col in range(diff.shape[1]):
+                    if diff[row, col] > 0:
+                        diff[row, col] = 1
+                        events.append([0, col, row, 0])
+
+        self.clipped_prev_frame = clipped_curr_frame
+
+        events == np.array(events)
+
+        return events, diff
+
+    def calc_density(self, events, bbox, shape, scale):
+        """
+            calculate the density within the bbox, in percentage (%).
+        """
+        cnt = 0
+
+        for event in events:
+            row = event[1]*scale
+            col = event[2]*scale
+            if row >= bbox["x_min"] and row <= bbox["x_max"] and \
+                col >= bbox["y_min"] and col <= bbox["y_max"]:
+                cnt += 1
+
+        bbox_area = (bbox["x_max"] - bbox["x_min"]) * (bbox["y_max"] - bbox["y_min"])
+        density = float(cnt)/bbox_area
+
+        return density*100
+
---
+++ edgaze/pipeline.py
@@ -0,0 +1,179 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+This is the pipeline interface to harness other modules in the eye segmentation stage
+
+@author: Yu Feng
+"""
+import os
+import sys
+
+# add this path to import other modules in the current directory
+sys.path.append(os.path.dirname(__file__))
+
+import atexit
+
+import bbox_predictor
+import bbox_sift
+import cv2
+import emulator
+import eye_segmentation
+import numpy
+import torch
+
+
+#%%
+
+FULL_DENSITY = 100
+
+
+class SmartCameraFilter(object):
+    def __init__(
+        self,
+        model_name,
+        model_path,
+        device,
+        scaledown,
+        record_path,
+        use_sift=False,
+        disable_edge_info=False,
+        bbox_model_path="model_weights/G030_c32_best.pth",
+        blur_size=3,
+        clip_val=10,
+        threshold_ratio=0.3,
+        density_threshold=0.05,
+        preview=False,
+    ):
+        """Init.
+
+        Args:
+            model_name: str, the name of eye segmentation model
+            model_path: str, loading path for model
+            device: str, device to run eye segmentation model
+            scaledown: int, the dimension scale down number
+            record_path: str, the result recording path
+            use_sift: bool, enable sift feature descriptor 
+                      to tracking the bbox instead of DNN model
+            disable_edge_info: bool, disable edge input to the bbox predictor
+            blur_size: the gaussian blur size apply to input images
+            clip_val: clip all low pixel value to this number
+            threshold_ratio: the threshold ratio to activate a event
+            density_threshold: the threshold to determine whether performing 
+                               DNN eye segmentation on current input image
+        """
+
+        self.simple_emulator = emulator.SimpleEmulator(
+            blur_size=blur_size, 
+            clip_val=clip_val, 
+            threshold_ratio=threshold_ratio
+        )
+
+        self.eye_segmentation = eye_segmentation.EyeSegmentation(
+            model_name=model_name, 
+            model_path=model_path, 
+            device=device
+        )
+
+        if not use_sift:
+            self.bbox_predictor = bbox_predictor.BBoxPredictor(
+                    device, scaledown, preview, 
+                    bbox_model_path=bbox_model_path,
+                    density_threshold=density_threshold,
+                    disable_edge_info=disable_edge_info)
+        else:
+            self.bbox_predictor = bbox_sift.BBoxPredictor(
+                    scaledown, preview, 
+                    density_threshold=density_threshold)
+
+        self.scaledown = scaledown
+        self.preview = preview
+        self.use_sift = use_sift
+
+        os.makedirs(record_path, exist_ok=True)
+
+        self.bbox_record_file = open(
+            os.path.join(record_path, "bbox_record_file.txt"), "w+"
+        )
+        self.warp_record = open(os.path.join(record_path, "warp_record.txt"), "w+")
+
+        atexit.register(self.cleanup)
+
+    def cleanup(self):
+        """cleanup and close files"""
+        self.bbox_record_file.close()
+        self.warp_record.close()
+
+    def downsize(self, img, size):
+        """downsample the image"""
+        img_shape = img.shape
+        dim = (img_shape[1] // size, img_shape[0] // size)
+        fx = 1.0 / size
+        fy = 1.0 / size
+        resized_img = cv2.resize(
+            src=img, dsize=dim, fx=fx, fy=fy, interpolation=cv2.INTER_AREA
+        )
+
+        return resized_img
+
+    def predict(self, framename):
+        """
+            predict eye segmentation
+            the main logic of the pipeline, first emulate events,
+            then, predict bounding box and whether to execute DNN,
+            lastly, predict eye segmentation result.
+        """
+
+        img = cv2.imread(framename, cv2.IMREAD_GRAYSCALE)
+        img = img.astype(numpy.float32)
+        img_no_resize = numpy.array(img)
+        img = self.downsize(img, self.scaledown)
+
+        img = img.astype(numpy.float32)
+        events, event_map = self.simple_emulator.generate_events(img)
+        if self.use_sift:
+            img = self.downsize(img_no_resize, self.scaledown)
+            new_bbox = self.bbox_predictor.predict(img)
+        else:
+            new_bbox = self.bbox_predictor.predict(event_map)
+
+        density = FULL_DENSITY
+        if new_bbox is not None:
+            density = self.simple_emulator.calc_density(
+                events, new_bbox, img.shape, self.scaledown
+            )
+        else:
+            new_bbox = {
+                "x_min": 0,
+                "x_max": img_no_resize.shape[1],
+                "y_min": 0,
+                "y_max": img_no_resize.shape[0],
+            }
+
+        self.bbox_record_file.write(
+            "%d %d %d %d\n"
+            % (
+                new_bbox["x_min"],
+                new_bbox["x_max"],
+                new_bbox["y_min"],
+                new_bbox["y_max"],
+            )
+        )
+
+        filter_result = self.eye_segmentation.predict_filtering(
+            img_no_resize.astype(numpy.uint8), new_bbox
+        )
+
+        able_warp, filter_result = self.bbox_predictor.update(filter_result, density)
+
+        if self.preview:
+            comb = numpy.hstack((img_no_resize/255., filter_result/4))
+            cv2.imshow("warpped & imference", comb)
+            cv2.waitKey(30)
+
+        self.warp_record.write("%f %s\n" % (density, able_warp))
+
+        filter_result = self.eye_segmentation.extract_pupil(
+            filter_result, "filter pupil"
+        )
+
+        return filter_result
